4ee7f174e3341810c0543be3f3aed68f3a985fb8
Philip Cleary <PclearyMISM@outlook.com>
2025-10-21 19:50:09 -0400

    Started styling the search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,14 @@
 import { Search } from "lucide-react";
 
 const SearchBar = () => {
-    return (
-        <div>
-            <Search />
-        </div>
-    )
-}
+  return (
+    <>
+      <button onClick={() => alert("Search Clicked.")}>
+        <Search />
+      </button>
+      <input className="px-4 py-2 rounded-md border border-gray-300 focus:outline-none" type="text" placeholder="Search..." />
+    </>
+  );
+};
 
 export default SearchBar;
