f284014efffc05f63eec4219c7c8bd7bb436468c
Philip Cleary <pcleary@usjersey.com>
2025-10-22 09:45:20 -0400

    Added useState to control the searchbar visibility.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,14 +1,22 @@
 import { Search } from "lucide-react";
+import { useState } from "react";
 
-const SearchBar = () => {
+export default function SearchBar() {
+
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
-    <>
-      <button onClick={() => alert("Search Clicked.")}>
-        <Search />
-      </button>
-      <input className="px-4 py-2 rounded-md border border-gray-300 focus:outline-none" type="text" placeholder="Search..." />
-    </>
-  );
-};
-
-export default SearchBar;
+    <div className="flex items-center gap-2">
+      {
+        showSearch &&
+        <input 
+        type="text" 
+        placeholder="Search..." 
+        className="px-4 py-2 rounded-md border border-1 border-gray-300 focus:outline-none focus:ring-2 focus:ring-red-600" />
+      }
+        <button onClick={() => setShowSearch(!showSearch)} className="cursor-pointer">
+          <Search />
+        </button>
+    </div>
+  )
+}
